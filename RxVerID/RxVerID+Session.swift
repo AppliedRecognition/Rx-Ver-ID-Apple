91dfee935bceaf5b1ad371ce6ac57e121b08b3ef
Jakub Dolejs <jakubdolejs@gmail.com>
2020-02-14 19:31:37 +0000

    Added methods to set factories on session

--- RxVerID/RxVerID+Session.swift
+++ RxVerID/RxVerID+Session.swift
@@ -33,33 +33,22 @@ fileprivate class SessionDelegate: VerIDSessionDelegate {
         self.event(.completed)
     }
     
-    
 }
 
 public extension RxVerID {
         
     // MARK: - Session
     
-    /// Run a Ver-ID session
+    /// Create and run a Ver-ID session
     /// - Parameter settings: Ver-ID session settings
     /// - Returns: Maybe whose value is a session result if the session completes successfully
     /// - Note: If the session is cancelled the maybe completes without a value. If the session fails the maybe returns an error.
     /// - Since: 1.0.0
     func session<T: VerIDSessionSettings>(settings: T) -> Maybe<VerIDSessionResult> {
-        return self.verid.asMaybe().flatMap { verid in
-            return Maybe<VerIDSessionResult>.create { maybe in
-                var delegate: SessionDelegate? = SessionDelegate(maybe)
-                let session = VerIDSession(environment: verid, settings: settings)
-                session.delegate = delegate
-                session.start()
-                return Disposables.create {
-                    delegate = nil
-                }
-            }
-        }.subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+        return self.session(settings: settings, translatedStrings: TranslatedStrings())
     }
     
-    /// Run a Ver-ID session
+    /// Create and run a Ver-ID session
     /// - Parameters:
     ///   - settings: Ver-ID session settings
     ///   - translatedStrings: Translations
@@ -67,17 +56,110 @@ public extension RxVerID {
     /// - Note: If the session is cancelled the maybe completes without a value. If the session fails the maybe returns an error.
     /// - Since: 1.1.0
     func session<T: VerIDSessionSettings>(settings: T, translatedStrings: TranslatedStrings) -> Maybe<VerIDSessionResult> {
-        return self.verid.asMaybe().flatMap { verid in
-            return Maybe<VerIDSessionResult>.create { maybe in
-                let session = VerIDSession(environment: verid, settings: settings, translatedStrings: translatedStrings)
-                var delegate: SessionDelegate? = SessionDelegate(maybe)
-                session.delegate = delegate
-                session.start()
-                return Disposables.create {
-                    delegate = nil
-                }
+        return self.createSession(settings: settings, translatedStrings: translatedStrings).asMaybe().flatMap({ session in
+            self.runSession(session)
+        }).subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+    }
+    
+    /// Run a Ver-ID session
+    /// - Parameter session: Session to run
+    /// - Since: 1.4.0
+    func runSession(_ session: VerIDSession) -> Maybe<VerIDSessionResult> {
+        return Maybe<VerIDSessionResult>.create(subscribe: { maybe in
+            var delegate: SessionDelegate? = SessionDelegate(maybe)
+            session.delegate = delegate
+            session.start()
+            return Disposables.create {
+                delegate = nil
             }
-        }.subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+        }).subscribeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+    }
+    
+    /// Create a Ver-ID session
+    /// - Parameter settings: Session settings
+    /// - Since: 1.4.0
+    func createSession<T: VerIDSessionSettings>(settings: T) -> Single<VerIDSession> {
+        return self.createSession(settings: settings, translatedStrings: TranslatedStrings())
+    }
+    
+    /// Create a Ver-ID session
+    /// - Parameters:
+    ///   - settings: Session settings
+    ///   - translatedStrings: Translations
+    /// - Since: 1.4.0
+    func createSession<T: VerIDSessionSettings>(settings: T, translatedStrings: TranslatedStrings) -> Single<VerIDSession> {
+        return self.verid.flatMap { verid in
+            return Single<VerIDSession>.create { event in
+                let session = VerIDSession(environment: verid, settings: settings, translatedStrings: translatedStrings)
+                event(.success(session))
+                return Disposables.create()
+            }
+        }
+    }
+    
+    /// Set video writer service factory on session
+    /// - Parameters:
+    ///   - videoWriterFactory: Video writer service factory
+    ///   - session: Session
+    /// - Since: 1.4.0
+    func setVideoWriterFactory(_ videoWriterFactory: VideoWriterServiceFactory, on session: VerIDSession) -> Single<VerIDSession> {
+        return Single<VerIDSession>.create { event in
+            session.videoWriterFactory = videoWriterFactory
+            event(.success(session))
+            return Disposables.create()
+        }
+    }
+    
+    /// Set face detection service factory on session
+    /// - Parameters:
+    ///   - faceDetectionFactory: Face detection service factory
+    ///   - session: Session
+    /// - Since: 1.4.0
+    func setFaceDetectionFactory(_ faceDetectionFactory: FaceDetectionServiceFactory, on session: VerIDSession) -> Single<VerIDSession> {
+        return Single<VerIDSession>.create { event in
+            session.faceDetectionFactory = faceDetectionFactory
+            event(.success(session))
+            return Disposables.create()
+        }
+    }
+    
+    /// Set result evaluation service factory on session
+    /// - Parameters:
+    ///   - resultEvaluationFactory: Result evaluation service factory
+    ///   - session: Session
+    /// - Since: 1.4.0
+    func setResultEvaluationFactory(_ resultEvaluationFactory: ResultEvaluationServiceFactory, on session: VerIDSession) -> Single<VerIDSession> {
+        return Single<VerIDSession>.create { event in
+            session.resultEvaluationFactory = resultEvaluationFactory
+            event(.success(session))
+            return Disposables.create()
+        }
+    }
+    
+    /// Set image writer service factory on session
+    /// - Parameters:
+    ///   - resultEvaluationFactory: Image writer service factory
+    ///   - session: Session
+    /// - Since: 1.4.0
+    func setImageWriterFactory(_ imageWriterFactory: ImageWriterServiceFactory, on session: VerIDSession) -> Single<VerIDSession> {
+        return Single<VerIDSession>.create { event in
+            session.imageWriterFactory = imageWriterFactory
+            event(.success(session))
+            return Disposables.create()
+        }
+    }
+    
+    /// Set session view controllers factory on session
+    /// - Parameters:
+    ///   - resultEvaluationFactory: Session view controllers factory
+    ///   - session: Session
+    /// - Since: 1.4.0
+    func setSessionViewControllersFactory(_ sessionViewControllersFactory: SessionViewControllersFactory, on session: VerIDSession) -> Single<VerIDSession> {
+        return Single<VerIDSession>.create { event in
+            session.sessionViewControllersFactory = sessionViewControllersFactory
+            event(.success(session))
+            return Disposables.create()
+        }
     }
 }
 #endif
